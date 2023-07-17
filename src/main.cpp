6fcf35aef28ecc4db901616fe401376bf638d46d
mrtuxa <mrtuxa@fedora.(none)>
2023-07-17 19:40:32 +0200

    CMakeLists.txt: better formatting
    main.cpp:
    Line 46 FUCK THIS PIECE OF SHIT

--- src/main.cpp
+++ src/main.cpp
@@ -26,55 +26,6 @@ int main() {
 
     client.on_log(dpp::utility::cout_logger());
 
-    client.on_message_create([&client](const dpp::message_create_t & event) {
-
-        std::stringstream ss(event.msg.content);
-        std::string command;
-
-        ss >> command;
-
-        /* Switch to or join the vc the user is on. Syntax: .join  */
-        if (command == ".join") {
-            dpp::guild * g = dpp::find_guild(event.msg.guild_id);
-            auto current_vc = event.from->get_voice(event.msg.guild_id);
-            bool join_vc = true;
-            /* Check if we are currently on any vc */
-            if (current_vc) {
-                /* Find the channel id  that the user is currently on */
-                auto users_vc = g->voice_members.find(event.msg.author.id);
-                /* See if we currently share a channel with the user */
-                if (users_vc != g->voice_members.end() && current_vc->channel_id == users_vc->second.channel_id) {
-                    join_vc = false;
-                    /* We are on this voice channel, at this point we can send any audio instantly to vc:
-
-                     * current_vc->send_audio_raw(...)
-                     */
-                } else {
-                    /* We are on a different voice channel. Leave it, then join the new one
-                     * by falling through to the join_vc branch below.
-                     */
-                    event.from->disconnect_voice(event.msg.guild_id);
-                    join_vc = true;
-                }
-            }
-            /* If we need to join a vc at all, join it here if join_vc == true */
-            if (join_vc) {
-                if (!g->connect_member_voice(event.msg.author.id)) {
-                    /* The user issuing the command is not on any voice channel, we can't do anything */
-                    client.message_create(dpp::message(event.msg.channel_id, "You don't seem to be on a voice channel! :("));
-                } else {
-                    /* We are now connecting to a vc. Wait for on_voice_ready
-                     * event, and then send the audio within that event:
-                     *
-                     * event.voice_client->send_audio_raw(...);
-                     *
-                     * NOTE: We can't instantly send audio, as we have to wait for
-                     * the connection to the voice server to be established!
-                     */
-                }
-            }
-        }
-    });
 
     client.on_slashcommand([&client](const dpp::slashcommand_t &event) {
         if (event.command.get_command_name() == "ping") {
@@ -91,6 +42,10 @@ int main() {
                     command_info()
             };
 
+            /**
+             * FUCK THIS PIECE OF SHIT
+             * IT'S not working
+             * ERROR: error creating slash commands: {"message": "Missing Access", "code": 50001}
             client.guild_bulk_command_create(commands, GUILD_ID, [&client](const dpp::confirmation_callback_t &event) {
                 if (event.is_error()) {
                     client.log(dpp::ll_error, "error creating slash commands: " + event.http_info.body);
@@ -98,6 +53,7 @@ int main() {
                     client.log(dpp::ll_info, "success creating slash commands");
                 }
             });
+            */
             client.global_bulk_command_create(commands, [&client](const dpp::confirmation_callback_t &event) {
                 if (event.is_error()) {
                     client.log(dpp::ll_error, "error creating slash commands: " + event.http_info.body);
